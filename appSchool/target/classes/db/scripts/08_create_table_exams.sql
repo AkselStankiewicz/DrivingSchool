21767e9bd549dbfdfa82883aba9616d5c3582a4c
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2024-04-17 22:15:45 +0200

    DrivingSchool

--- appSchool/target/classes/db/scripts/08_create_table_exams.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS exams (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    exam_type VARCHAR(128),
-    student_id INTEGER,
-    is_passed BOOLEAN DEFAULT FALSE,
-    score INTEGER DEFAULT 0,
-    description TEXT,
-    FOREIGN KEY(student_id) REFERENCES students(id)
-)
